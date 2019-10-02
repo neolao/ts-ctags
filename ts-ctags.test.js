7112e94e62384a98748e60f3df6170b777d21902
neolao <contact@neolao.com>
2019-10-02 21:28:01 +0000

    Add more tests

--- ts-ctags.test.js
+++ ts-ctags.test.js
@@ -18,5 +18,29 @@ describe("ts-ctags", () => {
         done();
       });
     });
+
+    it("should detect the constructor", done => {
+      exec("./ts-ctags.js -f- ./tests/SimpleClass.ts", (error, stdout) => {
+        const regex = buildRegexpTag("+ SimpleClass#constructor", '/^  public constructor() {$/;"', "m", 4);
+        expect(regex.test(stdout)).toBe(true);
+        done();
+      });
+    });
+
+    it("should detect the private property", done => {
+      exec("./ts-ctags.js -f- ./tests/SimpleClass.ts", (error, stdout) => {
+        const regex = buildRegexpTag("- SimpleClass#foo", '/^  private foo: string;$/;"', "p", 2);
+        expect(regex.test(stdout)).toBe(true);
+        done();
+      });
+    });
+
+    it("should detect the public method", done => {
+      exec("./ts-ctags.js -f- ./tests/SimpleClass.ts", (error, stdout) => {
+        const regex = buildRegexpTag("+ SimpleClass#getFoo", '/^  public getFoo(): string {$/;"', "m", 8);
+        expect(regex.test(stdout)).toBe(true);
+        done();
+      });
+    });
   });
 });
